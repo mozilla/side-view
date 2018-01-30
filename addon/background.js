f4bd785d3720101c1ec06e48ca09e1c560af7af9
Ian Bicking <ianb@colorstudy.com>
2018-01-30 17:37:35 -0600

    Switch to the Firefox Android User-Agent string

--- addon/background.js
+++ addon/background.js
@@ -1,3 +1,11 @@
+const USER_AGENT = "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0";
+// iOS:
+//   Mozilla/5.0 (iPhone; CPU iPhone OS 9_2 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/47.0.2526.70 Mobile/13C71 Safari/601.1.46
+// Firefox for Android:
+//   Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0
+// Chrome for Android:
+//   Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19
+
 browser.contextMenus.create({
   id: "open-in-sidebar",
   title: "Open in sidebar",
@@ -35,7 +43,7 @@ browser.webRequest.onBeforeSendHeaders.addListener(function (info) {
   for (let i = 0; i < headers.length; i++) {
     let name = headers[i].name.toLowerCase();
     if (name === 'user-agent') {
-      headers[i].value = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/47.0.2526.70 Mobile/13C71 Safari/601.1.46';
+      headers[i].value = USER_AGENT;
       return {"requestHeaders": headers};
     }
   }
