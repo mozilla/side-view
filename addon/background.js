62e54787a91e3936e26b3ec5440778eed21d3e9f
Ian Bicking <ianb@colorstudy.com>
2018-02-23 13:51:00 -0600

    Move the browsed-to page into an iframe Fixes #9 by including a Home button Fixes #5, sidebar title is no longer updated

--- addon/background.js
+++ addon/background.js
@@ -24,21 +24,22 @@ browser.contextMenus.create({
 browser.contextMenus.onClicked.addListener((info, tab) => {
   let url = info.linkUrl || tab.url;
   let title = info.linkText || tab.title || "Page";
-  browser.sidebarAction.open().then(() => {
-    return browser.sidebarAction.setPanel({
-      panel: url
-    });
-  }).then(() => {
-    return browser.sidebarAction.setTitle({title});
-  }).catch((error) => {
+  sidebarOpenAndSend({type: "browse", url}).catch((error) => {
     console.error("Error setting panel to page:", error);
   });
 });
 
+// This is a RequestFilter: https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/webRequest/RequestFilter
+// It matches tabs that aren't attached to a normal location (like a sidebar)
+// It only matches embedded iframes
+let requestFilter = {
+  tabId: -1,
+  types: ["sub_frame"],
+  urls: ["http://*/*", "https://*/*"],
+};
+
+// Add a mobile header to outgoing requests
 browser.webRequest.onBeforeSendHeaders.addListener(function (info) {
-  if (info.tabId !== -1) {
-    return;
-  }
   let headers = info.requestHeaders;
   for (let i = 0; i < headers.length; i++) {
     let name = headers[i].name.toLowerCase();
@@ -47,4 +48,46 @@ browser.webRequest.onBeforeSendHeaders.addListener(function (info) {
       return {"requestHeaders": headers};
     }
   }
-}, {tabId: -1, types: ["main_frame"], urls: ["http://*/*", "https://*/*"]}, ["blocking", "requestHeaders"]);
+}, requestFilter, ["blocking", "requestHeaders"]);
+
+// Remove X-Frame-Options to allow any page to be embedded in an iframe
+chrome.webRequest.onHeadersReceived.addListener(function (info) {
+  let headers = info.responseHeaders;
+  for (let i = 0; i < headers.length; i++) {
+    let name = headers[i].name.toLowerCase();
+    if (name === 'x-frame-options' || name === 'frame-options') {
+      headers.splice(i, 1);
+      return {"responseHeaders": headers};
+    }
+  }
+}, requestFilter, ["blocking", "responseHeaders"]);
+
+// Used to open the sidebar, then try to send a message.
+// This requires retries because the sidebar page takes a little time to load and respond to messages.
+function sidebarOpenAndSend(message) {
+  return browser.sidebarAction.open().then(() => {
+    return retry(() => {
+      return browser.runtime.sendMessage(message);
+    }, {times: 3, wait: 50});
+  });
+}
+
+function retry(attempter, options) {
+  let times = options.times || 3;
+  let wait = options.wait || 100;
+  return attempter().catch((error) => {
+    times--;
+    if (times <= 0) {
+      throw error;
+    }
+    return timeout(wait).then(() => {
+      retry(attempter, {times, wait});
+    });
+  });
+}
+
+function timeout(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
