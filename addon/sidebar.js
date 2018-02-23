7d5367ebf85d5140c09ddf6ec02bb2ffe96cdf88
Ian Bicking <ianb@colorstudy.com>
2018-02-23 17:38:51 -0600

    Fix #2, show a list of the current window's tabs in the sidebar home

--- addon/sidebar.js
+++ addon/sidebar.js
@@ -21,6 +21,29 @@ function displayPage(url) {
 function displayHome() {
   element("#browser-container").style.display = "none";
   element("#onboarding").style.display = "";
+  browser.windows.getCurrent({populate: true}).then((windowInfo) => {
+    let tabList = element("#tabs");
+    tabList.innerHTML = "";
+    for (let tab of windowInfo.tabs) {
+      let li = document.createElement("li");
+      let image = document.createElement("img");
+      image.src = tab.favIconUrl;
+      let anchor = document.createElement("a");
+      anchor.href = tab.url;
+      anchor.textContent = tab.title;
+      anchor.addEventListener("click", (event) => {
+        displayPage(event.target.href);
+        event.preventDefault();
+        return false;
+      })
+      anchor.prepend(image);
+      li.appendChild(anchor);
+      tabList.appendChild(li);
+    }
+    console.log("would set", windowInfo);
+  }).catch((error) => {
+    console.error("Error getting window info:", error);
+  });
 }
 
 function element(selector) {
@@ -35,3 +58,5 @@ let thisWindowId;
 browser.windows.getCurrent().then((windowInfo) => {
   thisWindowId = windowInfo.id;
 });
+
+displayHome();
