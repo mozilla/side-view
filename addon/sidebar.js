c4789f897e2c6da7e017437d7b8493ec3211593b
Ian Bicking <ianb@colorstudy.com>
2018-02-23 19:35:39 -0600

    Fix #8, allow user to request desktop view of a sidebar
    Desktop view is set on a per-hostname basis, and persisted via sync storage.

--- addon/sidebar.js
+++ addon/sidebar.js
@@ -4,18 +4,22 @@ browser.runtime.onMessage.addListener((message) => {
     return;
   }
   if (message.type == "browse") {
-    displayPage(message.url);
+    displayPage(message.url, message.desktop);
   } else {
     console.error("Got unexpected message:", message);
   }
 });
 
-function displayPage(url) {
+let lastDisplayedUrl;
+
+function displayPage(url, desktop) {
+  lastDisplayedUrl = url;
   element("#onboarding").style.display = "none";
   element("#browser-container").style.display = "";
   element("#browser-iframe").src = url;
   let urlObj = new URL(url);
   element("#browser-domain").textContent = urlObj.hostname;
+  element("#desktop").checked = !!desktop;
 }
 
 function displayHome() {
@@ -40,7 +44,6 @@ function displayHome() {
       li.appendChild(anchor);
       tabList.appendChild(li);
     }
-    console.log("would set", windowInfo);
   }).catch((error) => {
     console.error("Error getting window info:", error);
   });
@@ -52,6 +55,15 @@ function element(selector) {
 
 element("#home").addEventListener("click", () => {
   displayHome();
+});
+
+element("#desktop").addEventListener("change", (event) => {
+  let desktop = event.target.checked;
+  browser.runtime.sendMessage({type: "setDesktop", desktop, url: lastDisplayedUrl}).then(() => {
+    displayPage(lastDisplayedUrl, desktop);
+  }).catch((error) => {
+    console.error("Error handling desktop request:", error);
+  });
 });
 
 let thisWindowId;
