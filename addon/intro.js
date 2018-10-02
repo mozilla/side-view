b716680fdafbd46026668801fb310351a259b7be
Ian Bicking <ianb@colorstudy.com>
2018-10-02 14:42:42 -0500

    Fix #338, move onboarding into a popup

--- addon/intro.js
+++ addon/intro.js
@@ -4,34 +4,34 @@ let currentSlide = 0;
 function selectSlide(num) {
   for (let i = 0; i < foundSlides; i++) {
     let el = document.querySelector(`#slide-${i}`);
-    let selectButton = document.querySelector(`#select-${i}`);
     if (i === num) {
       el.classList.add("slide-displayed");
       el.classList.remove("slide-hidden");
-      selectButton.classList.add("selected");
     } else {
       el.classList.remove("slide-displayed");
       el.classList.add("slide-hidden");
-      selectButton.classList.remove("selected");
     }
   }
-  previous.disabled = (num === 0);
-  next.disabled = (num === foundSlides - 1);
+  previous.disabled = num === 0;
+  next.disabled = num === foundSlides - 1;
 }
 
 for (let el of document.querySelectorAll(".slide")) {
   let num = parseInt(el.id.replace(/[^\d]/g, ""), 10);
   foundSlides = Math.max(foundSlides + 1, num);
-  let button = document.querySelector(`#select-${num}`);
-  button.addEventListener("click", () => {
-    selectSlide(num);
-  });
 }
 
 let previous = document.querySelector("#previous");
 
 previous.addEventListener("click", () => {
   currentSlide = Math.max(0, currentSlide - 1);
+  if (currentSlide === 0) {
+    document.querySelector("#previous").classList.add("button-invisible");
+  }
+  if (currentSlide === foundSlides - 2) {
+    document.querySelector("#next").classList.remove("button-hidden");
+    document.querySelector("#done").classList.add("button-hidden");
+  }
   selectSlide(currentSlide);
 });
 
@@ -39,6 +39,13 @@ let next = document.querySelector("#next");
 
 next.addEventListener("click", () => {
   currentSlide = Math.min(foundSlides - 1, currentSlide + 1);
+  if (currentSlide === 1) {
+    document.querySelector("#previous").classList.remove("button-invisible");
+  }
+  if (currentSlide === foundSlides - 1) {
+    document.querySelector("#next").classList.add("button-hidden");
+    document.querySelector("#done").classList.remove("button-hidden");
+  }
   selectSlide(currentSlide);
 });
 
